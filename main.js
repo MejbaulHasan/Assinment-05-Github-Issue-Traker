5e4aabbb4aa921b821bfbc2409770c2bac026858
MejbaulHasan <mejbaulhasankhokon@gmail.com>
2026-03-09 23:07:07 +0600

    add all allIssue is mainJs file

--- main.js
+++ main.js
@@ -21,3 +21,85 @@ let allBtnArray = 0;
 let openBtnArray = 0;
 let closedBtnArray = 0;
 
+const allIssuesDisplay = (issues, id) => {
+  openBtnArray = 0;
+  closedBtnArray = 0;
+  allBtnArray = 0;
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  issues.forEach((issue) => {
+    if (issue.status == "open") {
+      openBtnArray++;
+    }
+    if (issue.status == "closed") {
+      closedBtnArray++;
+    }
+    if (issue.status == "open" || issue.status == "closed") {
+      allBtnArray++;
+    }
+    if (id === "all" || issue.status == id) {
+      const card = document.createElement("div");
+      card.innerHTML = `
+      
+        <div onclick="modalBtn('${issue.id}')" class="card bg-base-100 flex flex-col h-full shadow-sm  border-t-4 ${issue.status === "open" ? " border-green-400" : " border-purple-400"} ">
+              <div class="p-4">
+                <div class="flex justify-between items-center pb-2">
+                  <img  src= "${issue.status === "open" ? "assets/Open-Status.png" : "assets/Closed-Status.png"}" alt="" />
+
+                  <div class="${issue.priority.toUpperCase() === "HIGH" ? "bg-red-200" : issue.priority.toUpperCase() === "MEDIUM" ? "bg-yellow-200" : "bg-gray-200"} rounded-2xl px-6 py-1">
+
+                    <h1 class="text-[12px] ${issue.priority.toUpperCase() == "HIGH" ? "text-red-500" : issue.priority.toUpperCase() === "MEDIUM" ? "text-[#F59E0B]" : "text-gray-500"}">${issue.priority.toUpperCase()}</h1>
+                  </div>
+                </div>
+                <h1 class="font-semibold text-xl  pb-2 title line-clamp-1">
+                  ${issue.title}
+                </h1>
+                <p class="text-[#64748B]  pb-5 description text-sm line-clamp-1">
+                  ${issue.description}
+                </p>
+                <div class="flex flex-wrap items-center gap-2 pb-2 pt-5">
+                <div class="${
+                  issue.labels[0] === "enhancement"
+                    ? "bg-green-400 text-white"
+                    : "bg-red-200 text-red-500"
+                } rounded-2xl px-2 py-1 flex items-center gap-2">
+
+                <i class="${
+                  issue.labels[0] === "enhancement"
+                    ? "fa-solid fa-wand-magic-sparkles"
+                    : "fa-solid fa-bug"
+                }"></i>
+
+                <h1 class="text-[12px]">
+                  ${issue.labels[0] === undefined ? "No Bug" : issue.labels[0]}
+                </h1>
+
+               </div>
+
+          <div class="bg-[#FDE68A]/60 rounded-2xl px-2 py-1 flex items-center gap-2 
+          text-[#f18408] ${issue.labels[1] === undefined ? "hidden" : ""}">
+
+              <i class="fa-solid fa-life-ring"></i>
+          <h1 class="text-[12px]">
+          ${issue.labels[1] === undefined ? "" : issue.labels[1]}
+          </h1>
+
+            </div>
+
+            </div>
+                <hr class="border border-gray-300 mt-3 " />
+    
+                <div class="pt-2">
+                  <h1 class="text-[#64748B]">${issue.author}</h1>
+                  <h2 class="text-[#64748B]">${issue.createdAt}</h2>
+                </div>
+              </div>
+            </div> 
+        
+        `;
+      cardContainer.append(card);
+    }
+  });
+  manageLoader(false);
+  // console.log(openBtnArray);
+};
